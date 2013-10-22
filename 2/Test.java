be6a916981231f9efcbcf07027adba55a099c697
Abraham Hinteregger <oerpli@hotmail.com>
2013-10-23 01:16:46 +0200

    more final

--- 2/Test.java
+++ 2/Test.java
@@ -11,20 +11,22 @@ public class Test {
 		Random r = new Random();
 		String[] soils = { "sand", "humus", "clay" };
 		String[] sorts = { "hokkaido", "patisson", "aladdin" };
-		String[] tasks = { "water", "weed", "fertilize" };
+		String[] tasks = { "water", "weed", "fertilize", "harvest" };
 
 		User[] user = new User[25];
 		for (int i = 0; i < user.length; i++)
-			user[i] = User.register();
+			user[i] = User.register(); // 25 benutzer
 
 		Log.chatty = false; // feedback nach jedem task on/off
 
+		// jeweils 2 kürbisse (zufällige kombi) pro benutzer
 		for (int i = 0; i < user.length; i++)
 			user[i].task("plant", sorts[r.nextInt(3)], soils[r.nextInt(3)]);
 		Time.nextHour();
 		for (int i = 0; i < user.length; i++)
 			user[i].task("plant", sorts[r.nextInt(3)], soils[r.nextInt(3)]);
 
+		// die ersten 1500 stunden wird auch gedüngt, dann nicht mehr
 		for (int t = 0; t < 5300; t++) {
 			for (int i = 0; i < user.length; i++) {
 				if (t < 1500)
@@ -34,13 +36,14 @@ public class Test {
 			}
 			Time.nextHour();
 		}
+
 		Time.nextDay();
 		user[1].task("fertilize", 0);
-		while (Time.getDay() < Time.Tage) {
-			user[24].task("fertilize", 0); // Pumpkin will be rotten
+		while (Time.getDay() < Time.Tage) {// Nr. 24 düngt weiterhin
+			user[24].task("fertilize", 0); // Kürbis wird vergammeln
 			Time.nextDay();
 		}
-
+		// ranking der nicht vergammelten kürbisse
 		System.out.println(ContestManager.getRanking());
 	}
 }

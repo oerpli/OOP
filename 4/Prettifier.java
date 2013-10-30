66320c879092474e70fa506bcae6943c840ddd72
maisi <maisi1415@gmail.com>
2013-10-30 12:19:04 +0100

    Neue Aufgabe

---
+++ 4/Prettifier.java
@@ -0,0 +1,7 @@
+public interface Prettifier {
+      String pretty(String prog);
+      /* Result corresponds to the Java program in prog,
+       * but with more, less or prettier comments.
+       * This method has no side-effects.
+       */
+   }

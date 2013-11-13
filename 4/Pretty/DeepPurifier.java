9de3450f299dbf01d76bf880a3a7a2b41a26bf4a
Abraham Hinteregger <oerpli@hotmail.com>
2013-11-13 11:55:07 +0100

    test fix

--- 4/Pretty/DeepPurifier.java
+++ 4/Pretty/DeepPurifier.java
@@ -1,5 +1,15 @@
 package Pretty;
 
+/**
+ * Eigentlich problematisch da AltPurifier immer einen fixen Einzug hat und
+ * deshalb ein DeepPurifier Objekt mit Einzug X kommen könnte und das würde
+ * Probleme bei Prüfung geben.
+ * 
+ * Aus Zeitgründen nicht mehr repariert (in eine Überklasse abstrahiert etc...)
+ * 
+ * @author oerpli
+ * 
+ */
 public class DeepPurifier extends AltPurifier {
 	private int indent;
 

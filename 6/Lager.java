40ff5cd649fbfb34418ca582f06c730e90023d0a
e0942320 <e0942320@student.tuwien.ac.at>
2013-11-27 01:19:15 +0100

    Update Lager.java

--- 6/Lager.java
+++ 6/Lager.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
 
 class Lager {
-	ArrayList<Lagerplatz> listT20;
-	ArrayList<Lagerplatz> listT4;
-	ArrayList<Lagerplatz> listT_18;
-	ArrayList<Lagerplatz> listT_30;
+	Lagerplatz[] listT20;
+	Lagerplatz[] listT4;
+	Lagerplatz[] listT_18;
+	Lagerplatz[] listT_30;
 	
+	Lager(int T20, int T4, int T_18, int T_30) {
+		listT20 = new Lagerplatz[T20];
+		listT4 = new Lagerplatz[T4];
+		listT_18 = new Lagerplatz[T_18];
+		listT_30 = new Lagerplatz[T_30];
+	}
 }

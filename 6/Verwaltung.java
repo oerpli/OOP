8e3eafbfd0a2cdde330938c9afe1a1f428e8216f
e0942320 <e0942320@student.tuwien.ac.at>
2013-11-27 01:28:25 +0100

    Update Verwaltung.java

--- 6/Verwaltung.java
+++ 6/Verwaltung.java
@@ -1,3 +1,17 @@
 class Verwaltung {
-
+	public int store(Ware ware) {
+		return 5;
+	}
+	
+	public Ware store(int) {
+		return null;
+	}
+	
+	public String inventar() {
+		return "";
+	}
+	
+	public String utilization() {
+		return "";
+	}
 }

96522e9409ef944fddbb35d4aa27f165426e9438
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-05 00:15:44 +0100

    Update Bacterium.java

--- 7/Bacterium.java
+++ 7/Bacterium.java
@@ -1,6 +1,6 @@
 class Bacterium implements Runnable {
 	private int prolifNum; /** Zwischen 1 und 32 */
-	private Box container;
+	private Box container; /** Die Box in der sich das Bakterium befindet */
 	
 	public Bacterium(Box container, int prolifNum) {
 		this.container = container;
@@ -9,19 +9,34 @@ class Bacterium implements Runnable {
 	
 	@Override
 	public void run() {
-		
+		boolean goOn = true;
+		while (goOn) {
+			if (container.getNutrient() < 25) {
+				container.kill();
+				goOn = false; // Ist der Prozess beendet?
+			} else if (container.getNutrient() >= 75) {
+				Thread.sleep(5);
+				proliferate();
+			} else if (container.getNutrient() >= 50) {
+				Thread.sleep(5);
+				proliferate();
+			} else { // zwischen 25 und 50
+				Thread.sleep(5);
+				proliferate();
+			}
+		}
 	}
 	
 	private void proliferate() {
 		Box[] neighbors = container.getNeighbors();
 		neighbors.sort(); // nach Nährstoff absteigend sortieren
 		for (Box b: neighbors) { // passendes Feld für Teilung finden:
-			if (b.getNutrient >= 25 && !b.nearFungus() // Nachbar darf kein Pilz sein
+			if (b.getNutrient() >= 25 && !b.nearFungus() // Nachbar darf kein Pilz sein
 				&& !b.isFungus() && !b.isBacterium()) { // Feld darf nicht besetzt sein
 				prolifNum++;
 				Bacterium child = Bacterium(b, prolifNum);
-				
-				// TODO: Bacterium in Box eintragen
+				container.consumNutrient();
+				// TODO: Bacterium in Box eintragen, Box Methoden schreiben
 			}
 		}
 	}

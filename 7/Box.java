e086ba109c9a4b5dabf047aa5207620f6c932bdf
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-04 23:29:19 +0100

    Update Box.java

--- 7/Box.java
+++ 7/Box.java
@@ -2,4 +2,8 @@ class Box {
 	private int nutrient; /** Zwischen 0 und 100 */
 	private Bacterium bac;
 	private Fungus myk; /** Eines von beiden immer Null */
+	
+	public int getNutrient() {
+		return nutrient;
+	}
 }

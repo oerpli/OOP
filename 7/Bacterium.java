be2fa48c3e985b9608bb5fda35eae52dc429d79f
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-06 19:34:08 +0100

    Update Bacterium.java

--- 7/Bacterium.java
+++ 7/Bacterium.java
@@ -1,8 +1,10 @@
 class Bacterium implements Runnable {
+	private Playground playground;
 	private int prolifNum; /** Zwischen 1 und 32 */
 	private Box container; /** Die Box in der sich das Bakterium befindet */
 	
-	public Bacterium(Box container, int prolifNum) {
+	public Bacterium(Playground playground, Box container, int prolifNum) {
+		this.playground = playground;
 		this.container = container;
 		this.prolifNum = prolifNum;
 	}

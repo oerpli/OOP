058cd1689e217b214addf454ac4e6a8077926be9
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-06 19:33:58 +0100

    Update Fungus.java

--- 7/Fungus.java
+++ 7/Fungus.java
@@ -1,8 +1,10 @@
 class Fungus implements Runnable {
+	private Playground playground;
 	private int prolifNum; /** Zwischen 1 und 32 */
 	private Box container; /** Die Box in der sich das Bakterium befindet */
 	
-	public Fungus(Box container, int prolifNum) {
+	public Fungus(Playground playground, Box container, int prolifNum) {
+		this.playground = playground;
 		this.container = container;
 		this.prolifNum = prolifNum;
 	}

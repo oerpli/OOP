beaca0b9b15719b4f0be5063c6d4a9adbfa08484
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-07 23:21:17 +0100

    Update Box.java

--- 7/Box.java
+++ 7/Box.java
@@ -17,12 +17,6 @@ class Box {
 		nutrient = (int)(nutrient - (nutrient * (consumption / 100.0)));
 	}
 	
-	public void kill() {
-		bacterium = null;
-		fungus = null;
-		// TODO: Prozess beenden.
-	}
-	
 	public boolean isTaken() {
 		if (bacterium == null && fungus == null) return false;
 		return true;
@@ -32,15 +26,26 @@ class Box {
 		return false;
 	}
 	
+	/**
+	 * Gibt die umliegenden 3 - 8 Boxen als Array aus.
+	 */
 	public Box[] getNeighbors() {
 		
 	}
 	
+	/**
+	 * Siedelt die übergebene Zelle in der Box an. Alte Zellen werden gelöscht.
+	 * Der Übergabewert null leert die Box.
+	 */
 	public void setResident(Bacterium resident) {
 		bacterium = resident;
 		fungus = null;
 	}
 	
+	/**
+	 * Siedelt die übergebene Zelle in der Box an. Alte Zellen werden gelöscht.
+	 * Der Übergabewert null leert die Box.
+	 */
 	public void setResident(Fungus resident) {
 		fungus = resident;
 		bacterium = null;

93854d0abbc8171338bdd98f15b8acdd59f25f58
maisi <maisi1415@gmail.com>
2013-12-11 00:43:02 +0100

    a

--- 7/Test.java
+++ 7/Test.java
@@ -2,6 +2,7 @@ public class Test {
 	public static void main(String[] args) {
 			int[] time = { 30, 20, 10 };
 			Playground pg = new Playground(50, 50, 5, time);
+			pg.createCell("Bacterium", 55, 55);
 			pg.createCell("Bacterium", 0, 0);
 			pg.createCell("Bacterium", 1, 8);
 			pg.createCell("Bacterium", 2, 7);
@@ -9,6 +10,8 @@ public class Test {
 			pg.createCell("Bacterium", 4, 5);
 			
 			pg.createCell("Bacterium", 50, 50);
+			
+			
 
 	}
 }

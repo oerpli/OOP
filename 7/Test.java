44a22e0534b1d690d7e545729f79178169bb1dcf
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-11 00:30:34 +0100

    Update Test.java

--- 7/Test.java
+++ 7/Test.java
@@ -2,6 +2,16 @@ public class Test {
 	public static void main(String[] args) {
 			int[] time = { 30, 20, 10 };
 			Playground pg = new Playground(50, 50, 5, time);
-			pg.start();	
+			Box bbox = pg.getBox(0, 0);
+			Bacterium bac = new Bacterium(pg, bbox, 1);
+			Bacterium[] bacArr = { bac };
+			Box fbox = pg.getBox(25, 20);
+			Fungus bmyk = new Fungus(pg, fbox, 1);
+			Fungus[] mykArr = { myk };
+			pg.start(bacArr, mykArr);
+			pg.makeRandomCell();
+			pg.makeRandomCell();
+			pg.makeRandomCell();
+			pg.makeRandomCell();
 	}
 }

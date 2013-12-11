1c3e2a0d90c1ba3f2e8e660db45a361ee8ef03c4
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-11 21:03:48 +0100

    Update Pumpkin.java

--- 8/Pumpkin.java
+++ 8/Pumpkin.java
@@ -1,3 +1,16 @@
 abstract class Pumpkin {
+	private int weight;
+	private int waterLevel;
 	
+	public void simuRain() {
+		
+	}
+	
+	public void simuSun() {
+		
+	}
+	
+	public void simuWind() {
+		
+	}
 }

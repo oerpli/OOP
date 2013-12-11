78f15bf1841cd5d414ef5cfc95e916a6f988d62a
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-11 01:43:08 +0100

    Update Test.java

--- 7/Test.java
+++ 7/Test.java
@@ -5,13 +5,36 @@ public class Test {
 			pg.createCell("Bacterium", 55, 55);
 			pg.createCell("Bacterium", 0, 0);
 			pg.createCell("Bacterium", 1, 8);
+			pg.createCell("Fungus", 2, 5);
 			pg.createCell("Bacterium", 2, 7);
 			pg.createCell("Bacterium", 3, 6);
 			pg.createCell("Bacterium", 4, 5);
+			pg.createCell("Fungus", 40, 40);
 			
+			Thread.sleep(10000);
+			
+			time = { 50, 30, 4 };
+			pg = new Playground(70, 70, 5, time);
+			pg.createCell("Bacterium", 55, 55);
+			pg.createCell("Bacterium", 1, 0);
+			pg.createCell("Bacterium", 15, 32);
+			pg.createCell("Fungus", 2, 5);
 			pg.createCell("Bacterium", 50, 50);
+			pg.createCell("Bacterium", 67, 67);
+			pg.createCell("Bacterium", 4, 5);
+			pg.createCell("Fungus", 10, 68);
 			
+			Thread.sleep(10000);
 			
-
+			time = { 50, 30, 4 };
+			pg = new Playground(80, 80, 5, time);
+			pg.createCell("Bacterium", 50, 50);
+			pg.createCell("Bacterium", 79, 79);
+			pg.createCell("Bacterium", 15, 32);
+			pg.createCell("Fungus", 2, 5);
+			pg.createCell("Bacterium", 10, 70);
+			pg.createCell("Bacterium", 67, 47);
+			pg.createCell("Bacterium", 4, 7);
+			pg.createCell("Fungus", 50, 78);
 	}
 }

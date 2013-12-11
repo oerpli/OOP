cbc63a1229bbc05bf915e0ecaa04b5705c4d0d1b
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-11 21:26:27 +0100

    Update Test.java

--- 8/Test.java
+++ 8/Test.java
@@ -1,5 +1,7 @@
 class Test {
 	public static void main(String[] args) {
-		
+		ArrayList<Pumpkin> arrList = new ArrayList<Pumpkin>();
+		Pumpkin[] arr = Pumpkin[6];
+		Hashtable<Integer, Pumpkin> hashTable = new Hashtable<Integer, Pumpkin>();
 	}
 }

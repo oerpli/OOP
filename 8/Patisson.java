6ef51136273078301a88dfd623812a09c4efa43f
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-11 21:06:46 +0100

    Update Patisson.java

--- 8/Patisson.java
+++ 8/Patisson.java
@@ -1,3 +1,16 @@
 class Patisson extends Pumpkin {
-  
+	@Override
+	public void simuRain() {
+		
+	}
+	
+	@Override
+	public void simuSun() {
+		
+	}
+	
+	@Override
+	public void simuWind() {
+		
+	}
 }

3d59a93cd988f176dd57c20814b2e91a69e7b371
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-11 21:06:12 +0100

    Update Hokkaido.java

--- 8/Hokkaido.java
+++ 8/Hokkaido.java
@@ -1,3 +1,16 @@
 class Hokkaido extends Pumpkin {
+	@Override
+	public void simuRain() {
+		
+	}
 	
+	@Override
+	public void simuSun() {
+		
+	}
+	
+	@Override
+	public void simuWind() {
+		
+	}
 }

1d7b625411c61a05eca426a326e5a85e84d0445c
Abraham Hinteregger <oerpli@hotmail.com>
2013-12-17 11:59:37 +0100

    Revert "annotation nur für klassen"
    This reverts commit d2da2409dff0f2e6e6d1449eac1daff3d2fdfe94.

--- 8/WorkedOnAnnotation.java
+++ 8/WorkedOnAnnotation.java
@@ -2,17 +2,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-@WorkedOnAnnotation(worker1 = "Maisriemler", comment1 = "", comment2 = "", worker2 = "")
-@Target(ElementType.TYPE)
+@WorkedOnAnnotation(worker1="Maisriemler", comment1 = "", comment2 = "", worker2 = "")
 @Retention(value = RetentionPolicy.SOURCE)
 public @interface WorkedOnAnnotation {
 
 	String worker1();
-
 	String comment1();
-
 	String worker2();
-
 	String comment2();
 }

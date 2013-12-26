c0674147a3d5e1a5f1fdb707fa03f3155a8c3661
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-26 19:00:29 +0100

    Update Cocktail.java

--- 9/Cocktail.java
+++ 9/Cocktail.java
@@ -5,4 +5,39 @@ class Cocktail {
 	// Alkoholgehalt wird berechnet
 	float ice;
 	int temperature;
+	
+	/**
+	 * @param ingredients: Array of up to 3 Liquors
+	 * @param ice: non negative amount of crushed ice
+	 */
+	public Cocktail(String name, Liquor[] ingredients, float ice, int temperature) {
+		this.name = name;
+		this.ingredients = ingredients;
+		this.ice = ice;
+		this.temperature = temperature;
+	}
+	
+	/**
+	 * Returns total volume of this Cocktail (millilitres)
+	 * This calculation does not consider ice
+	 */
+	public int getVolume() {
+		int result = 0;
+		for (Liquor l: ingredients) {
+			result += l.getVolume();
+		}
+		return result;
+	}
+	
+	/**
+	 * Returns alcohol concentration
+	 * This calculation does not consider ice
+	 */
+	public float getAbV() {
+		float result = 0.0f;
+		for (Liquor l: ingredients) {
+			result += l.getVolume() * (l.getAbV() / 100);
+		}
+		result = result / getVolume();
+	}
 }

3628d319f3cb511377dada109109c9b69786e0ff
e0942320 <e0942320@student.tuwien.ac.at>
2013-12-26 17:43:10 +0100

    Create Liquor.java

---
+++ 9/Liquor.java
@@ -0,0 +1,17 @@
+class Liquor {
+	private String name;
+	private float alcByVol;
+	private float volume;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public float getAbV() {
+		return alcByVol;
+	}
+	
+	public float getVolume() {
+		return volume;
+	}
+}

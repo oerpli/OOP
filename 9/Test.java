188d80bea9823905caa51a6b4d5944bacbe530b5
e0942320 <e0942320@student.tuwien.ac.at>
2014-01-02 16:48:17 +0100

    Update Test.java

--- 9/Test.java
+++ 9/Test.java
@@ -4,12 +4,12 @@ public class Test {
 		// Gib aktuell verfügbare Cocktails aus
 		System.out.println(bar.toString());
 		
-		ArrayList<String> order = new ArrayList<String>();
+		String[] order = String[4];
 		order.add("Cuba Libre");
 		order.add("Cuba Libre");
 		order.add("Mojito");
 		order.add("Swimming Pool");
 		// Bestelle die Cocktails
-		ArrayList<???> tray = bar.order(order);
+		Cocktail[] tray = bar.order(order);
 	}
 }

75cd27321a5ef7aefbf9b82f1000ab5aec0664e4
e0942320 <e0942320@student.tuwien.ac.at>
2014-01-03 01:56:26 +0100

    Create Order.java

---
+++ 9/Order.java
@@ -0,0 +1,19 @@
+class Order {
+  String[] cocktails;
+  
+  public Order(String[] cocktails) {
+    this.cocktails = cocktails;
+  }
+  
+  /**
+	 *
+	 */
+  public void list() {
+    // die nächste Methode die besser als toString funktionieren würde
+  }
+  
+  @Override
+  public String toString() {
+    
+  }
+}

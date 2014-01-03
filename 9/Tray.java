f2ead648e8522250fd24fc917c11492489cec1df
e0942320 <e0942320@student.tuwien.ac.at>
2014-01-03 01:52:11 +0100

    Create Tray.java

---
+++ 9/Tray.java
@@ -0,0 +1,24 @@
+class Tray {
+	ArrayList<Cocktail> cocktails;
+	
+	public Tray() {
+		cocktails = new ArrayList<Cocktail>;
+	}
+	
+	public void add(Cocktail cocktail) {
+		
+	}
+
+	/**
+	 *
+	 */
+	public void content() {
+		// Komische Methode... toString wäre dafür besser geeignet...
+		// aber verlangt
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
+}

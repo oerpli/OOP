ef4f70f3edbe92a68ddf80cb4a874dc5c5a8d761
Abraham Hinteregger <oerpli@hotmail.com>
2014-01-07 14:23:47 +0100

    Aspect zum Zählen implementiert

--- 9/Test.java
+++ 9/Test.java
@@ -1,13 +1,21 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.err.println("start");
 		Bar b = Bar.getInstance();
+		end();
+		test(b);
+		end();
+	}
+
+	public static void test(Bar b) {
 		Order o = new Order(1);
 		o.add("Mojito").add("Cuba Libre").add("Bier");
 		Tray t = b.order(o);
 		o.list();
 		t.content();
+	}
+
+	public static void end() {
 	}
 
 }
